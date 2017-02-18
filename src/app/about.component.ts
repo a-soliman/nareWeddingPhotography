70513b6db64a82efaa16930736c9436aabb5c0d4
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-02-17 18:08:03 -0800

    added the commponent class

---
+++ src/app/about.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { SocialIconsComponent } from './social-icons.component';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
